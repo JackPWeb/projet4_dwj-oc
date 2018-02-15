087b43cfed222da25880d464f14c2585d0bd9a90
JackPWeb <contact@jackpweb.fr>
2018-02-15 10:35:27 +0100

    maj db

--- core/Database/MysqlDatabase.php
+++ core/Database/MysqlDatabase.php
@@ -12,15 +12,15 @@
 		private $pdo;
 
 		public function __construct($db_name, $db_user ='root', $db_pass='', $db_host = 'localhost'){
-			$this->$db_name = $db_name;
-			$this->$db_user = $db_user;
-			$this->$db_pass = $db_pass;
-			$this->$db_host = $db_host;
+			$this->db_name = $db_name;
+			$this->db_user = $db_user;
+			$this->db_pass = $db_pass;
+			$this->db_host = $db_host;
 		}
 
 		private function getPDO(){
 			if ($this->pdo === null) {
-				$pdo = new PDO('mysql:dbname=oc_projet4-2;host=localhost', 'root', '');
+				$pdo = new PDO("mysql:dbname=$this->db_name;host=$this->db_host", $this->db_user, $this->db_pass);
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				$this->pdo = $pdo;
 			}
